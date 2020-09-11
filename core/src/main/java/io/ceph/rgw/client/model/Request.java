7775228b6c79a0ae47b2d74b192cdcc6f8fb1195
ss <sosozhuang@163.com>
2020-09-11 19:23:48 +0800

    Feat: add super class of request and response
    Signed-off-by: ss <sosozhuang@163.com>

---
+++ core/src/main/java/io/ceph/rgw/client/model/Request.java
@@ -0,0 +1,14 @@
+package io.ceph.rgw.client.model;
+
+/**
+ * Root class of other request, every request has a corresponding {@link Response}.
+ *
+ * @author zhuangshuo
+ * Created by zhuangshuo on 2020/3/7.
+ * @see BucketRequest
+ * @see ObjectRequest
+ * @see io.ceph.rgw.client.model.admin.AdminRequest
+ * @see io.ceph.rgw.client.model.notification.SubscribeRequest
+ */
+public interface Request {
+}
---
+++ core/src/main/java/io/ceph/rgw/client/model/Response.java
@@ -0,0 +1,14 @@
+package io.ceph.rgw.client.model;
+
+/**
+ * Root class of other response, every response has a corresponding {@link Request}.
+ *
+ * @author zhuangshuo
+ * Created by zhuangshuo on 2020/3/7.
+ * @see BucketResponse
+ * @see ObjectResponse
+ * @see io.ceph.rgw.client.model.admin.AdminResponse
+ * @see io.ceph.rgw.client.model.notification.SubscribeResponse
+ */
+public interface Response {
+}
