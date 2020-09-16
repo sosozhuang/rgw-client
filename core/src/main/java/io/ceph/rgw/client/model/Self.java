ec5bcad47d23c84ebae6ac8ff63327f6271ae300
ss <sosozhuang@163.com>
2020-09-16 14:59:26 +0800

    Fix: type param extends from Self
    Signed-off-by: ss <sosozhuang@163.com>

--- core/src/main/java/io/ceph/rgw/client/model/Self.java
+++ core/src/main/java/io/ceph/rgw/client/model/Self.java
@@ -4,7 +4,12 @@ package io.ceph.rgw.client.model;
  * @author zhuangshuo
  * Created by zhuangshuo on 2020/3/16.
  */
-abstract class Self<T> {
+abstract class Self<T extends Self> {
+    /**
+     * cast to the object type
+     *
+     * @return the object type reference
+     */
     @SuppressWarnings("unchecked")
     protected T self() {
         return (T) this;
